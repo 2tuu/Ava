c7c22b8d7e5b1b4ca6e5955b3919ed87fa424966
Kab <39675015+2tuu@users.noreply.github.com>
2020-03-08 17:08:39 -0700

    Updated e621/e926 to adhere to new API structure

--- commands/e621.js
+++ commands/e621.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 var axios = require("axios");
-
 const cooldown = new Set();
+const config = require("./../config.json");
 
 exports.run = (client, message, args) => {
 	if(message.channel.nsfw === false){
@@ -20,17 +20,17 @@ exports.run = (client, message, args) => {
 		}, 5000);
 
 		async function apiGet(){
-			var request = await axios.get(`https://e621.net/post/index.json?tags=-order:random,-gore,${args.join(",")}`);
-			
-			if(!request.data[0]){
-				message.channel.send("No results, make sure you're using less than 6 tags");
-			} else {
+			var request = await axios.get(`https://e621.net/posts.json?limit=5&tags=order:random+-cub+-gore+${args.join("+")}`);
+			//console.log(request.data.posts);
 
-			var result = request.data[Math.floor(Math.random() * request.data.length)];
+			if(!request.data.posts[0]){
+				message.channel.send("No results, make sure you're using less than 4 tags");
+			} else {
+			var result = request.data.posts[0];
 			
 			const embed = new Discord.RichEmbed()
-			 .setImage(result.file_url)
-			 .setDescription(`Artist: ${result.artist.join(', ')}\n[link](${result.file_url})`)
+			 .setImage(result.file.url)
+			 .setDescription(`Artist: ${result.tags.artist.join(', ')}\n[link](https://e621.net/posts/${result.id})`)
 			 .setTimestamp()
 			 message.channel.send({embed});
 			
@@ -48,7 +48,7 @@ exports.run = (client, message, args) => {
 exports.conf = {
     help: "Pull an image from e621",
     format: "k?e621 {tags}",
-    DM: true,
-    OwnerOnly: true,
+    DM: false,
+    OwnerOnly: false,
     alias: []
 }
