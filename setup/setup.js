e4c92df36b8cb7681bd4c6244632069d98aa0bed
Kab <39675015+2tuu@users.noreply.github.com>
2021-04-29 17:24:03 -0700

    working on installation junk

--- setup/setup.js
+++ setup/setup.js
@@ -9,79 +9,47 @@ var connection = mysql.createConnection({
   });
 
   connection.query('CREATE TABLE announce (guild TEXT, channel TEXT);', function(err, rows){
-    if(err) {
-     throw "SQL Error: " + err;
-    }
     console.log('Created "announce" table successfully');
   });
 
   connection.query('CREATE TABLE blacklist (userid TEXT, reason TEXT);', function(err, rows){
-    if(err) {
-     throw "SQL Error: " + err;
-    }
     console.log('Created "blacklist" table successfully');
   });
 
     connection.query('CREATE TABLE modlog (logKicks TEXT, logReactions TEXT, logChannels TEXT, logEmojis TEXT, logBans TEXT, logLeaves TEXT, logMembers TEXT, logMessages TEXT, logRoles TEXT, serverId TEXT, enabled TEXT, channel TEXT);', function(err, rows){
-    if(err) {
-     throw "SQL Error: " + err;
-    }
     console.log('Created "modlog" table successfully');
   });
 
   connection.query('CREATE TABLE note (note BLOB, ownerId TEXT);', function(err, rows){
-    if(err) {
-     throw "SQL Error: " + err;
-    }
     console.log('Created "note" table successfully');
   });
 
   connection.query('CREATE TABLE nsfw (id TEXT, birthyear TEXT);', function(err, rows){
-    if(err) {
-     throw "SQL Error: " + err;
-    }
     console.log('Created "nsfw" table successfully');
   });
 
-  connection.query('CREATE TABLE prefix (prefix TEXT, welcomeMessage TEXT, welcomeChannel TEXT, shouldWelcome TEXT, serverId TEXT);', function(err, rows){
-    if(err) {
-     throw "SQL Error: " + err;
-    }
+  connection.query('CREATE TABLE prefixes (prefix TEXT, welcomeMessage TEXT, welcomeChannel TEXT, shouldWelcome TEXT, serverId TEXT);', function(err, rows){
     console.log('Created "prefixes" table successfully');
   });
 
-  connection.query('CREATE TABLE profile (userId TEXT, quarters TEXT, badge TEXT, desc TEXT, color TEXT, cmds TEXT, qTime TEXT);', function(err, rows){
-    if(err) {
-     throw "SQL Error: " + err;
-    }
+  connection.query('CREATE TABLE profile (prefix TEXT);', function(err, rows){
     console.log('Created "profile" table successfully');
   });
 
+
   connection.query('CREATE TABLE prefix (prefix TEXT, welcomeMessage TEXT, welcomeChannel TEXT, shouldWelcome TEXT, serverId TEXT);', function(err, rows){
-    if(err) {
-     throw "SQL Error: " + err;
-    }
     console.log('Created "prefixes" table successfully');
   });
 
   connection.query('CREATE TABLE settings (serverId TEXT, banId TEXT);', function(err, rows){
-    if(err) {
-     throw "SQL Error: " + err;
-    }
     console.log('Created "settings" table successfully');
   });
 
   connection.query('CREATE TABLE tags (serverID TEXT, tagName TEXT, tagContent TEXT, ownerID TEXT, selfDelete TEXT);', function(err, rows){
-    if(err) {
-     throw "SQL Error: " + err;
-    }
     console.log('Created "tags" table successfully');
   });
 
   connection.query('CREATE TABLE timer (endtime TEXT, user TEXT, channelcreated TEXT, message TEXT);', function(err, rows){
-    if(err) {
-     throw "SQL Error: " + err;
-    }
     console.log('Created "time" table successfully');
   });
 
