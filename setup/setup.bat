c3a846e788958fe8b29ad90b38ef211142b85312
Kab <39675015+2tuu@users.noreply.github.com>
2021-04-30 17:40:27 -0700

    Still working on this install script

--- setup/setup.bat
+++ setup/setup.bat
@@ -1,6 +1,6 @@
-@echo off
+call npm i mysql
+@echo on
 echo Setting up MySQL database...
 node setup.js
-echo Creating junk folder...
 echo Install complete - PLEASE CREATE A FOLDER NAMED 'commands-locked' NEXT TO THE 'commands' FOLDER.
 pause
