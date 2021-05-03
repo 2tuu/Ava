95def7726d4ec299fcb6964d0801058db43e4adf
Kab <39675015+2tuu@users.noreply.github.com>
2021-05-03 16:04:13 -0700

    fixed db bug

--- events/message.js
+++ events/message.js
@@ -5,14 +5,14 @@ let config = JSON.parse(fs.readFileSync("./config.json", "utf8"));
 exports.run = (deletedMessage, pool, client, message) => {
       if(!message.guild) return;
       pool.query(`SELECT * FROM prefixes WHERE serverId ='${message.guild.id}'`).then(row => {
-        if(!row.rows){
+        if(!row.rows[0]){
           pool.query(`INSERT INTO prefixes (prefix, welcomeMessage, welcomeChannel, shouldWelcome, serverId) VALUES ('${config.prefix}', 'This is a placeholder welcome message', 'null', 'false', '${message.guild.id}')`);
           console.log("added to prefixes");
         }
       });
 
       pool.query(`SELECT * FROM announce WHERE guild ='${message.guild.id}'`).then(row => {
-        if(!row.rows){
+        if(!row.rows[0]){
           pool.query(`INSERT INTO announce (guild, channel) VALUES ('${message.guild.id}', null)`);
           console.log("added to announcement");
         }
