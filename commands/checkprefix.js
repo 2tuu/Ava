f07c26a0a21299ab97ce143bd59edfd27e10c87c
Kab <39675015+2tuu@users.noreply.github.com>
2021-05-03 16:03:32 -0700

    fixed for new db

--- commands/checkprefix.js
+++ commands/checkprefix.js
@@ -3,12 +3,13 @@ const Discord = require("discord.js");
 exports.run = (client, message, args, deletedMessage, sql) => {
    
 
-  sql.get(`SELECT * FROM prefixes WHERE serverId ="${message.guild.id}"`).then(row => {
+  sql.query(`SELECT * FROM prefixes WHERE serverId ='${message.guild.id}'`).then(row => {
+    row = row.rows;
 
-    if(!row){
+    if(!row[0]){
       var prefix = "k?";
     } else {
-      var prefix = row.prefix;
+      var prefix = row[0].prefix;
     }
 
     const embed = new Discord.MessageEmbed()
