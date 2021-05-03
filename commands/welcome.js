3c7d6eec9fe30f36e54925ccc4f382b8434c3a75
Kab <39675015+2tuu@users.noreply.github.com>
2021-05-02 17:07:06 -0700

    working on fixing for new db

--- commands/welcome.js
+++ commands/welcome.js
@@ -20,30 +20,31 @@ if(message.member.permissions.has('BAN_MEMBERS')){
 			const embed = new Discord.MessageEmbed()
 		.setDescription("Welcome channel set to Channel ID: " + channelID)
 		message.channel.send({embed});
-		sql.run(`UPDATE prefixes SET welcomeChannel = "${channelID}" WHERE serverId = ${message.guild.id}`);
+		sql.query(`UPDATE prefixes SET welcomeChannel = '${channelID}' WHERE serverId = '${message.guild.id}'`);
 		}
 
 	} else if(args[0] === "edit"){
 		const embed = new Discord.MessageEmbed()
 		.setDescription("Welcome message set to: " + args.slice(1).join(' '))
 		message.channel.send({embed});
-		sql.run(`UPDATE prefixes SET welcomeMessage = "${args.slice(1).join(' ')}" WHERE serverId = ${message.guild.id}`);
+		sql.query(`UPDATE prefixes SET welcomeMessage = '${args.slice(1).join(' ')}' WHERE serverId = '${message.guild.id}'`);
 	} else if(args[0] === "toggle"){
 
-		sql.get(`SELECT * FROM prefixes WHERE serverId ="${message.guild.id}"`).then(row => {
+		sql.query(`SELECT * FROM prefixes WHERE serverId ='${message.guild.id}'`).then(row => {
+			row = row.rows[0];
 			
-			if(row.shouldWelcome === "true"){
+			if(row.shouldwelcome === "true"){
 				const embed = new Discord.MessageEmbed()
 		.setColor(0xF46242)
 		.setDescription("Welcome messages disabled")
 		message.channel.send({embed});
-		sql.run(`UPDATE prefixes SET shouldWelcome = "false" WHERE serverId = ${message.guild.id}`);
+		sql.query(`UPDATE prefixes SET shouldWelcome = 'false' WHERE serverId = '${message.guild.id}'`);
 			} else {
 				const embed = new Discord.MessageEmbed()
 		.setColor(0x32ff58)
 		.setDescription("Welcome messages enabled")
 		message.channel.send({embed});
-		sql.run(`UPDATE prefixes SET shouldWelcome = "true" WHERE serverId = ${message.guild.id}`);
+		sql.query(`UPDATE prefixes SET shouldWelcome = 'true' WHERE serverId = '${message.guild.id}'`);
 			}
 				
 				}).catch(() => {
@@ -73,7 +74,7 @@ exports.conf = {
 	help: "Configure guild welcome messages",
 	format: "k?welcome toggle\nk?welcome edit\nk?welcome setchannel\n\n{member} is replaced with @user\n{member.username} is replaced with the user's username\n{guild} is replaced with the server's name",
 	DM: false,
-	OwnerOnly: false,
+	OwnerOnly: true,
 	alias: []
 }
 
