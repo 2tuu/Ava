2aee5dc841b86b54ab263d0d4e3ab65ac1c914df
Kab <39675015+2tuu@users.noreply.github.com>
2021-05-05 17:54:51 -0700

    updating query format

--- setup/setup.js
+++ setup/setup.js
@@ -23,12 +23,12 @@ const connection = new Pool({
   max: 0
 });
 
-connection.query('CREATE TABLE announce (guild TEXT, channel TEXT);', function(err, rows){
+connection.query('CREATE TABLE announce (guild TEXT, channel TEXT);').then(row=>{
   if(err) console.error('ERROR:\n'+err);
   console.log('Created "announce" table successfully');
 });
 
-connection.query('CREATE TABLE blacklist (userid TEXT, reason TEXT);', function(err, rows){
+connection.query('CREATE TABLE blacklist (userid TEXT, reason TEXT);').then(row=>{
   if(err) console.error('ERROR:\n'+err);
   console.log('Created "blacklist" table successfully');
 });
@@ -38,43 +38,43 @@ connection.query('CREATE TABLE modlog (logkicks TEXT, logreactions TEXT, logchan
   console.log('Created "modlog" table successfully');
 });
 
-connection.query('CREATE TABLE note (note TEXT, ownerid TEXT);', function(err, rows){
+connection.query('CREATE TABLE note (note TEXT, ownerid TEXT);').then(row=>{
   if(err) console.error('ERROR:\n'+err);
   console.log('Created "note" table successfully');
 });
 
-connection.query('CREATE TABLE nsfw (id TEXT, birthyear TEXT);', function(err, rows){
+connection.query('CREATE TABLE nsfw (id TEXT, birthyear TEXT);').then(row=>{
   if(err) console.error('ERROR:\n'+err);
   console.log('Created "nsfw" table successfully');
 });
 
-connection.query('CREATE TABLE prefixes (prefix TEXT, welcomeMessage TEXT, welcomeChannel TEXT, shouldWelcome TEXT, serverId TEXT);', function(err, rows){
+connection.query('CREATE TABLE prefixes (prefix TEXT, welcomeMessage TEXT, welcomeChannel TEXT, shouldWelcome TEXT, serverId TEXT);').then(row=>{
   if(err) console.error('ERROR:\n'+err);
   console.log('Created "prefixes" table successfully');
 });
 
-connection.query('CREATE TABLE profile (prefix TEXT);', function(err, rows){
+connection.query('CREATE TABLE profile (prefix TEXT);').then(row=>{
   if(err) console.error('ERROR:\n'+err);
   console.log('Created "profile" table successfully');
 });
 
 
-connection.query('CREATE TABLE prefix (prefix TEXT, welcomeMessage TEXT, welcomeChannel TEXT, shouldWelcome TEXT, serverId TEXT);', function(err, rows){
+connection.query('CREATE TABLE prefix (prefix TEXT, welcomeMessage TEXT, welcomeChannel TEXT, shouldWelcome TEXT, serverId TEXT);').then(row=>{
   if(err) console.error('ERROR:\n'+err);
   console.log('Created "prefixes" table successfully');
 });
 
-connection.query('CREATE TABLE settings (serverid TEXT, banid TEXT);', function(err, rows){
+connection.query('CREATE TABLE settings (serverid TEXT, banid TEXT);').then(row=>{
   if(err) console.error('ERROR:\n'+err);
   console.log('Created "settings" table successfully');
 });
 
-connection.query('CREATE TABLE tags (serverID TEXT, tagName TEXT, tagContent TEXT, ownerID TEXT, selfDelete TEXT);', function(err, rows){
+connection.query('CREATE TABLE tags (serverID TEXT, tagName TEXT, tagContent TEXT, ownerID TEXT, selfDelete TEXT);').then(row=>{
   if(err) console.error('ERROR:\n'+err);
   console.log('Created "tags" table successfully');
 });
                                             //user is a reserved term
-connection.query('CREATE TABLE timer (endtime TEXT, "user" TEXT, channelcreated TEXT, message TEXT);', function(err, rows){
+connection.query('CREATE TABLE timer (endtime TEXT, "user" TEXT, channelcreated TEXT, message TEXT);').then(row=>{
   if(err) console.error('ERROR:\n'+err);
   console.log('Created "time" table successfully');
 });
