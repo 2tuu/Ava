40a13a4701fb45b00eb3bdb0d521b6c65c04cdf8
Kab <39675015+2tuu@users.noreply.github.com>
2021-05-06 17:38:11 -0700

    Tied to config instead of being hardcoded

--- commands/invite.js
+++ commands/invite.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 
 exports.run = (client, message, args) => {
     const embed = new Discord.MessageEmbed()
-        .setDescription("Invite me to your server [here](https://discord.com/api/oauth2/authorize?client_id=435855803363360779&permissions=469888150&redirect_uri=https%3A%2F%2Fbot.kitk.us%2F&scope=bot)")
+        .setDescription(`Invite me to your server [here](${config.invite})`)
 
     message.channel.send({embed});
 }
