a338dc5a8f3c5debc899f5b328e6987050637896
Kab <39675015+2tuu@users.noreply.github.com>
2021-05-06 17:30:31 -0700

    Cleanup

--- commands/announcement.js
+++ commands/announcement.js
@@ -1,10 +1,10 @@
-//https://github.com/2tuu/Kit/blob/master/docs/news.md
 const Discord = require("discord.js");
+
+//temporary (?) accouncement command
 
 exports.run = (client, message, args) => {
     const embed = new Discord.MessageEmbed()
         .setDescription("Development announcements can be found [here](https://github.com/2tuu/Kit/blob/master/docs/news.md)")
-
     message.channel.send({embed});
 }
 
