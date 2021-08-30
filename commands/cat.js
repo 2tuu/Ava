a5ea88545de855bb7df06e4225fec50b5d45443d
Kab <39675015+2tuu@users.noreply.github.com>
2021-08-29 17:01:22 -0700

    API testing

--- commands/cat.js
+++ commands/cat.js
@@ -1,30 +1,24 @@
-const Discord = require("discord.js");
-const nekoClient = require('nekos.life');
-const neko = new nekoClient();
+const Discord = require('discord.js');
+const api = require('./../plugins/api.js');
 
-exports.run = (client, message, args) => {
-    async function getCat(){
-	var hug = await neko.sfw.meow();
-	var hugVar = JSON.stringify(hug.url);
-	hugVar = hugVar.replace(`"`,``);
-	hugVar = hugVar.replace(`"`,``);
+exports.run = async (client, message, args) => {
 
-	const embed = new Discord.MessageEmbed()
-	   .setTimestamp()
-	   .setImage(hugVar)
-	   .setFooter("Powered by Nekos.life")
-	 message.channel.send({embed});
-    console.log('\x1b[36m', 'MW: ' + hugVar);
-}
 
-getCat();
 
-}
+var action = "cat";
+    
+        var url = await api.getV(action);
+        const embed = new Discord.MessageEmbed()
+       .setImage(url.url)
+        message.channel.send({embed});
+    }
+    
+    exports.conf = {
+        help: "meow",
+        format: "k?cats",
+        DM: true,
+        OwnerOnly: true,
+        alias: ['cat']
+    }
 
-exports.conf = {
-    help: "view a cat",
-    format: "k?cat",
-    DM: true,
-    OwnerOnly: false,
-    alias: ["cac", "meow", "mewo", "mew", "kitty", "kitten", "nya", "nyan", "nyah"]
-}
+    //disabled until API replacement
