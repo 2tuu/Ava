89ed47d9547c21886e1c8f60af367a8e426def2f
Kab <39675015+2tuu@users.noreply.github.com>
2021-09-05 08:16:30 -0700

    nothing

--- commands/announcement.js
+++ commands/announcement.js
@@ -4,7 +4,7 @@ const Discord = require("discord.js");
 
 exports.run = (client, message, args) => {
     const embed = new Discord.MessageEmbed()
-        .setDescription("Development announcements can be found [here](https://github.com/2tuu/Kit/blob/master/docs/news.md)")
+        .setDescription("Development announcements/news can be found [here](https://github.com/2tuu/Kit/blob/master/docs/news.md)")
     message.channel.send({embed});
 }
 
