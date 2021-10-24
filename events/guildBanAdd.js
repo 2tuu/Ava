fbae74a94478b842d1831301559d6a789cc7a121
Kab <39675015+2tuu@users.noreply.github.com>
2021-10-24 07:50:01 -0700

    WIP executor logging

--- events/guildBanAdd.js
+++ events/guildBanAdd.js
@@ -1,6 +1,7 @@
 const Discord = require(`discord.js`);
 
 exports.run = async (deletedMessage, sql, client, guild, user) => {
+    
 
     try{
     var guildID = guild.id;
@@ -12,9 +13,7 @@ exports.run = async (deletedMessage, sql, client, guild, user) => {
 
     var row = await sql.query(`SELECT * FROM modlog WHERE serverid ='${guildID}'`);
 
-        var audit = await guild.fetchAuditLogs();
-
-
+        var audit = await guild.fetchAuditLogs(22);
 
         try{
         var auditLog = await audit.entries.first();
@@ -24,16 +23,42 @@ exports.run = async (deletedMessage, sql, client, guild, user) => {
             return ch.send("```diff\n-An error occured when accessing the audit log, please make sure I have permission to view it" + "\n```")
         }
 
+        //start new garbage
+
+        var currentTime = Date.now();
+        var auditTime = audit.entries.first().createdTimestamp;
+      
+        var wasKick = false;
+        var wasRecent = false;
+      
+        var diffe = currentTime - auditTime;
+      
+            if(diffe > 1000){
+              wasKick = false;
+            } else {
+              waskick = true;
+              wasRecent = true;
+            }
+      
+            if(audit.entries.first().target.id === member.id && wasRecent === true){
+              wasKick = true;
+            } else {
+              wasKick = false;
+            }
+
+        //end new garbage
 
         if(auditLog.reason){
         reason = auditLog.reason;
         }
+
+        var executor = auditLog.executor.tag;
         
         if(!row) return;
 
         if(row.enabled === "yes" && row.logbans === "yes"){
             var ch = client.guilds.cache.get(guildID).channels.cache.get(row.channel);
-            ch.send("```diff\n-Member Banned: " + user.tag + `\nReason: ${reason}` + "\n```")
+            ch.send("```diff\n-Member Banned: " + user.tag + `\nReason: ${reason}` + "\nExecutor: " + executor + "```")
         }
 
 }
