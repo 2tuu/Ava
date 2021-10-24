73dc3c33a665b8124bf2a45d7aee45a374c92f61
Kab <39675015+2tuu@users.noreply.github.com>
2021-10-24 07:54:32 -0700

    added basic command statistics

--- commands/info.js
+++ commands/info.js
@@ -13,19 +13,22 @@ exports.run = (client, message) => {
 
 	k = k.username + "#" + k.discriminator;
 
+	var stats = [];
+	Object.keys(client.commandStats).forEach(key => {stats.push(key+ ': ' + client.commandStats[key])});
+	stats = stats.join('\n');
+
 
 	const embed = new Discord.MessageEmbed()
 	.setTitle("")
 	.setAuthor(client.user.username, client.user.avatarURL)
 
 	.setDescription(
-		"**Author: **" + `${k}` + "\n" + 
+		"**Stats: **" + `${k}` + "\n" + 
 		"**Guilds/Users: **" + `${client.guilds.cache.size}/${client.users.cache.size}` + "\n" +
-		"**Icon Source: ** " + data.icon_artist + " ([Link](" + data.icon_link + "))" + "\n" +
-		"**Github:** " + "[Link](https://github.com/2tuu/Kit)"
+		"**Stats:**\n" + stats
 	)
 
-	.setFooter('v.' + `${client.version} ${client.codename}` + ' (current: ' + client.currentVersion + ')')
+	.setFooter('v.' + `${client.version} ${client.codename}` + ' (current repo version: ' + client.currentVersion + ')')
 
 	message.channel.send({embed})
 
@@ -34,9 +37,10 @@ exports.run = (client, message) => {
  }
  
  exports.conf = {
+	name: "N/A (dev command)",
     help: "View my version information, the artist for my avatar, and other statistics",
     format: "k?info",
     DM: true,
-    OwnerOnly: false,
+    OwnerOnly: true,
     alias: ['stats']
 }
