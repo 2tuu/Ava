4514eeb4646ec4cfc4b5a026f593e0debc9d3731
Kab <39675015+2tuu@users.noreply.github.com>
2021-11-04 15:32:39 -0700

    updated format, weird api bug workaround

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -6,7 +6,12 @@ exports.run = (deletedMessage, sql, client, guild) => {
     //Log channel variable
     const logChannel = client.channels.cache.get(config.logChannel);
 
-    console.log(">>>Guild Left/Connection Lost: " + guild.name + " (" + guild.id + ")");
-    logChannel.send("```diff\n>>>Guild Left/Connection Lost: " + guild.name + " (" + guild.id + ")\n->>>" + client.guilds.cache.size + " Servers\n```");
+    //circumventing weird bug
+    if(!guild.name){
+        return;
+    } else {
+        logChannel.send("```diff\n>>>Guild Left/Connection Lost: " + guild.name + " (" + guild.id + ")\n->>>" + client.guilds.cache.size + " Servers\n```");
+        console.log(">>>Guild Left/Connection Lost: " + guild.name + " (" + guild.id + ")");
+    }
 
 }
