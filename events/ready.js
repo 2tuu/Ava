31c5d111dfbaa0db5cae3eb672ade69894a1a2ce
Kab <39675015+2tuu@users.noreply.github.com>
2021-11-10 17:49:46 -0800

    colored console outputs

--- events/ready.js
+++ events/ready.js
@@ -16,21 +16,21 @@ exports.run = async (deletedMessage, pool, client) => {
 		client.version = version.version;
 		client.codename = version.codename;
 
-	console.log (`Loaded ${client.totalCommands} modules - ${client.failedCommands.length} failed`)
-	console.log('\x1b[32m', "======================\n");
+	console.log ('\x1b[32m', `Loaded ${client.totalCommands} modules - ${client.failedCommands.length} failed`);
+	console.log('\x1b[34m%s\x1b[0m', "======================");
 	if(parseInt(current) > parseInt(client.version) && parseInt(client.version) !== 0){
 		console.error('Your framework is out of date');
 		console.error(`The current Github version is: ` + current + '\n');
 		console.error(`Your version is: ` + client.version + '\n');
-		console.error('\n To disable this notice, set your version number in /plugins/update.json to 0');
-		console.log('\x1b[32m', "======================");
+		console.error('\x1b[36m%s\x1b[0m','\n To disable this notice, set your version number in /plugins/update.json to 0');
+		console.log('\x1b[34m%s\x1b[0m', "======================");
 	} else if(client.version == '0'){
 		//disable version report
 	} else {
-		console.log('\x1b[33m', `Version: ` + client.version + ' - ' + client.codename + '\n');
+		console.log('\x1b[36m%s\x1b[0m', `Version: ` + client.version + ' - ' + client.codename);
 	}
-	console.log('\x1b[33m', `${client.users.cache.size} users - ${client.channels.cache.size} channels - ${client.guilds.cache.size} guilds`);
-	console.log('\x1b[32m', "=========log==========");
+	console.log('\x1b[36m%s\x1b[0m', `${client.users.cache.size} users - ${client.channels.cache.size} channels - ${client.guilds.cache.size} guilds`);
+	console.log('\x1b[34m%s\x1b[0m', "=========log==========");
     
 	client.user.setStatus('dnd');
 
@@ -51,9 +51,9 @@ exports.run = async (deletedMessage, pool, client) => {
 	try{
 		client.blacklist = await pool.query(`SELECT * FROM blacklist`);
 		client.blacklist = client.blacklist.rows.map(g=>g.userid);
-		console.log('Fetched blacklist');
+		console.log('\x1b[32m','Fetched blacklist');
 	} catch(err) {
-		console.error(err);
+		console.error(`Problem fetching blacklist: ${err}`);
 	}
 
 	cron.schedule('* * * * *', async function() {
