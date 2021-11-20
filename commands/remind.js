040869ab3ef63f23eafefdea491399e1931880f8
Kab <39675015+2tuu@users.noreply.github.com>
2021-11-20 15:08:49 -0800

    Revising code/replacing timestamps with new discord timestamps

--- commands/remind.js
+++ commands/remind.js
@@ -2,10 +2,19 @@ const { jsonfy } = require("booru/dist/Utils");
 
 exports.run = async (client, message, args, deletedMessage, sql) => {
 
+    function msToTime(ms) {
+        let minutes = (ms / (1000 * 60)).toFixed(1);
+        let hours = (ms / (1000 * 60 * 60)).toFixed(1);
+        let days = (ms / (1000 * 60 * 60 * 24)).toFixed(1);
+        if (minutes < 60) return minutes + "m";
+        else if (hours < 24) return hours + "h";
+        else return days + "d"
+      }
+
     var user = message.author.id;
     var channel = message.channel.id;
 
-    if(args[0] === "cancel"){
+    if(args[0] === "cancel" && !args[1]){
         var row = await sql.query(`SELECT * FROM timer WHERE "user" ='${message.author.id}'`);
             row = row.rows[0];
         if(!row){ //no timers exist
@@ -14,49 +23,66 @@ exports.run = async (client, message, args, deletedMessage, sql) => {
             sql.query(`DELETE FROM timer WHERE "user" ='${message.author.id}'`);
             return message.channel.send("Your timer has been deleted");
         }
-    }
-
-    if(isNaN(args[0])){
-        return message.channel.send("Please enter a valid number of minutes");
-    } else if(!args[1]){ //minute given but no message
-        return message.channel.send("Please give me a message for your reminder.");
-    } else if(!isNaN(args[1]) && !args[2]){ //hour given but no message
-        return message.channel.send("Please give me a message for your reminder.");
-    }
-
-    //usage: k?remind [minutes] [hours] message
-
-        var minutes = args[0] * 60000;
-        var hours = 0;
-
-        if(!isNaN(args[1])){
-           hours = args[1] * 3600000;
+    } else if(args[0] === "check"){
+        //sql
+        var row = await sql.query(`SELECT * FROM timer WHERE "user" ='${user}'`);
+        row = row.rows[0];
+        if(!row){ //no timers exist
+            return message.channel.send("You don't have any timers running");
+        } else { //timer already exists (limit 1 per user)
+            return message.channel.send(`You have a timer expiring <t:${Math.round(row.endtime/1000)}:R>`);
         }
+    }
 
-        var time = minutes+hours;
 
-        if(time/60000 == 1){
-            var min = 'minute';
-        } else if(time < 0 || time > 604800001){
-            return message.channel.send("Please enter a time between 0 minutes and 7 days");
-        } else {
-            var min = 'minutes';
+    if(args.join(' ').match(/-t (.*)/g)){
+
+        function getSeconds(str) {
+            var days = str.match(/(\d+)\s*d/);
+            var hours = str.match(/(\d+)\s*h/);
+            var minutes = str.match(/(\d+)\s*m/);
+
+            if(!hours) hours = ['0h'];
+            if(!minutes) minutes = ['0m'];
+            if(!days) days = ['0d'];
+
+            return {
+                m: minutes[0].replace('m',''),
+                h: hours[0].replace('h',''),
+                d: days[0].replace('d','')
+            }
+          }
+
+        var num = args.join(' ').match(/-t (.*)/g)[0].replace('-t ', '');
+        num = getSeconds(num.toString());
+    } else {
+        return message.channel.send("The time wasn't set correctly, please refer to `k?help remind` for proper format")
+    }
+
+
+
+
+        var minutes = num.m * 60000;
+        var hours = num.h * 3600000;
+        var days = num.d * 86400000;
+
+        var time = minutes+hours+days;
+
+        if(time < 59999 || time > 604800001){
+            return message.channel.send("Please enter a time between 1 minute and 7 days");
         }
 
         var endtime = Date.now() + time;
         var timerMessage;
 
-        if(isNaN(args[1])){
-            timerMessage = args.slice(1).join(' ');
-        } else {
-            timerMessage = args.slice(2).join(' ');
-        }
+        timerMessage = args.join(' ').split('-t')[0];
+        timerMessage = timerMessage.substring(0, timerMessage.length - 1);
 
         if(timerMessage.length > 1921){
             return message.channel.send("Please give me a message below 1,920 characters");
         }
 
-        message.channel.send(`I will tell you: \`${timerMessage}\` in ${time/60000} ${min}`);
+        message.channel.send(`I will tell you: \`${timerMessage}\` <t:${Math.round(endtime/1000)}:R>`);
 
         //sql
         var row = await sql.query(`SELECT * FROM timer WHERE "user" ='${user}'`);
@@ -64,17 +90,16 @@ exports.run = async (client, message, args, deletedMessage, sql) => {
         if(!row){ //no timers exist
             sql.query(`INSERT INTO timer (endtime, "user", channelcreated, message) VALUES ('${endtime}', '${user}', '${channel}', '${timerMessage}')`);
         } else { //timer already exists (limit 1 per user)
-            var timerEnd = Date.now();
-                timerEnd = row.endtime - timerEnd;
-            message.channel.send(`You already have a timer expiring in ${Math.round(timerEnd/60000) + 0.4} ${min}`);
+            message.channel.send(`You already have a timer expiring <t:${Math.round(row.endtime/1000)}:R>`);
         }
 
 }
 
 exports.conf = {
+    category: "Utility",
     name: "Remind",
     help: "Remind yourself of something in the future",
-    format: "k?remind [minutes] {optional: hours}",
+    format: "k?remind <message> -t #h #m\nk?remind cancel",
     DM: true,
     OwnerOnly: true,
     alias: []
