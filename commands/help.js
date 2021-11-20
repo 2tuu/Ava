96f9836aec05aff30cb6d3360ba7802173d1d325
Kab <39675015+2tuu@users.noreply.github.com>
2021-11-20 15:09:27 -0800

    Added categories to help command

--- commands/help.js
+++ commands/help.js
@@ -3,13 +3,48 @@ const Discord = require("discord.js");
 exports.run = (client, message, args) => {
 
     var commandList = Object.keys(client.help);
+    //console.log(commandList);
+    var list = {};
+
+    Object.keys(client.help).forEach(function(key) {
+        if(list[client.help[key].category]){
+            list[client.help[key].category].push(client.help[key].filename);
+        } else {
+            list[client.help[key].category] = [client.help[key].filename];
+        }
+      });
+
+    client.help.forEach(e=>{
+        if(list[e.category]){
+            list[e.category].push(e.filename);
+        } else {
+            list[e.category] = [e.filename];
+        }
+    });
+
+    var preFinal = {};
+    Object.keys(list).forEach(function(key) {
+        preFinal[key] = list[key].join(', ');
+    });
+
+    //console.log(preFinal);
+
+    var final;
+    Object.keys(preFinal).forEach(function(key) {
+        final = final + `**${key}**\n\`\`\``;
+        final = final + preFinal[key];
+        final = final + '```\n'
+    });
+
+    //console.log(final);
+
     
     if(!args[0]){
 
         const embed = new Discord.MessageEmbed()
         .setTitle("Command documentation")
-        .setDescription("Use this command to view what a command does\n\nArguments surrounded by [] are required, ones surrounded by {} are optional\n")
-        .addField('Commands', '```' + commandList.join(', ') + '```')
+        .setDescription("Use this command to view what a command does\n\nArguments surrounded by [] are required, ones surrounded by {} are optional\n"+
+                        "\n" + final.slice(9))
         message.channel.send({embed});
 
     } else if(commandList.includes(args[0].toLowerCase())){
@@ -27,6 +62,7 @@ exports.run = (client, message, args) => {
 }
 
 exports.conf = {
+    category: "Utility",
     name: "Help",
     help: "Why do you need help with this one?",
     format: "k?help [command]",
