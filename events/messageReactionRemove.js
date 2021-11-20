7f3f26b547fd2d25a8f2f643c25f5028af1046ca
Kab <39675015+2tuu@users.noreply.github.com>
2021-11-20 15:06:21 -0800

    Phasing this out soon

--- events/messageReactionRemove.js
+++ events/messageReactionRemove.js
@@ -1,5 +1,7 @@
+const Discord = require(`discord.js`);
 exports.run = (deletedMessage, sql, client, messageReaction, user) => {
 
+    /* phase this junk out entirely
     try{
         var guildID = messageReaction.message.guild.id;
         } catch(err){
@@ -9,12 +11,18 @@ exports.run = (deletedMessage, sql, client, messageReaction, user) => {
         sql.query(`SELECT * FROM modlog WHERE serverid ='${guildID}'`).then(row => {
             row = row.rows[0];
             if(!row) return;    
+
+            
     
             if(row.enabled === "yes" && row.logreactions === "yes"){
                var ch = client.guilds.cache.get(guildID).channels.cache.get(row.channel);
-               ch.send("```diff\n-Reaction Removed:\n" + `User: ${user.tag}\nEmoji: ${messageReaction.emoji.name}\nMessage: \n+${messageReaction.message.author.tag}: ${messageReaction.message.content}`  + "```")
+               
+            const embed = new Discord.MessageEmbed()
+            .setColor(0xFF4D00)
+            .setDescription("```diff\n-Reaction Removed:\n" + `User: ${user.tag}\nEmoji: ${messageReaction.emoji.name}\nMessage: \n+${messageReaction.message.author.tag}: ${messageReaction.message.content}`  + "```")
+            return ch.send({embed});
             }
     
         });
-
+*/
 }
