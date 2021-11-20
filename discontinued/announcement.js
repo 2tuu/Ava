89aa7d50b676f8470b7326d3e65740d6ba58699d
Kab <39675015+2tuu@users.noreply.github.com>
2021-11-20 15:07:30 -0800

    Getting rid of this

---
+++ discontinued/announcement.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+
+//temporary (?) accouncement command
+
+exports.run = (client, message, args) => {
+    const embed = new Discord.MessageEmbed()
+        .setDescription("Development announcements/news can be found [here](https://github.com/2tuu/Kit/blob/master/docs/news.md)")
+    message.channel.send({embed});
+}
+
+exports.conf = {
+    category: "Development",
+    name: "Utility",
+    help: "Temporary announcement command",
+    format: "n/a",
+    DM: true,
+    OwnerOnly: false,
+    alias: []
+}
