897f6aab0e85822f1242798d1b8c4d22582300a7
Kab <39675015+2tuu@users.noreply.github.com>
2021-11-24 16:22:12 -0800

    Added some subcommands to blacklist

--- commands/blacklist.js
+++ commands/blacklist.js
@@ -3,7 +3,57 @@ const Discord = require('discord.js');
 //TODO fix this junk
 exports.run = async (client, message, args, deletedMessage, sql) => {
 
-    var user = client.users.cache.get(args[0]);
+    if(args[0].toLowerCase() === 'lookup'){
+        sql.query(`SELECT * FROM blacklist WHERE userid ='${args[1]}'`).then(row=>{
+            row = row.rows[0];
+            if(!row){
+                const embed = new Discord.MessageEmbed()
+                .setColor(`0x${client.colors.bad}`)
+                .setDescription("No results")
+                return message.channel.send({embed});
+            } else {
+                const embed = new Discord.MessageEmbed()
+                .setColor(`0x${client.colors.neutral}`)
+                .setDescription(`${row.userid} was blacklisted for "${row.reason}"`)
+                return message.channel.send({embed});
+            }
+        }).catch((err) => {
+            return message.channel.send('Database error:\n```js\n' + err + '\n```');
+        });
+    } else if (args[0].toLowerCase() === "list"){
+        sql.query(`SELECT * FROM blacklist`).then(row=>{
+            row = row.rows;
+            if(row.length < 1){
+                const embed = new Discord.MessageEmbed()
+                .setColor(`0x${client.colors.bad}`)
+                .setDescription("No results")
+                return message.channel.send({embed});
+            } else {
+                var res = [];
+
+                row.forEach(e => {
+                    res.push(`${e.userid} - ${e.reason}`)
+                });
+
+                const embed = new Discord.MessageEmbed()
+                .setColor(`0x${client.colors.neutral}`)
+                .setDescription(res)
+                return message.channel.send({embed});
+            }
+        }).catch((err) => {
+            return message.channel.send('Database error:\n```js\n' + err + '\n```');
+        });
+    } else if(args[0].toLowerCase() === "delete"){
+        sql.query(`DELETE FROM blacklist WHERE userid ='${args[1]}'`).then(row=>{
+            row = row.rows;
+                const embed = new Discord.MessageEmbed()
+                .setColor(`0x${client.colors.neutral}`)
+                .setDescription(`Deleting ${args[1]} from blacklist`)
+                return message.channel.send({embed});
+        }).catch((err) => {
+            return message.channel.send('Database error:\n```js\n' + err + '\n```');
+        });
+    } else {
 
     var reason = 'No reason';
     if(args[1]){
@@ -20,6 +70,7 @@ exports.run = async (client, message, args, deletedMessage, sql) => {
     sql.query(`INSERT INTO blacklist (userid, reason) VALUES (${args[0]}, '${reason}')`).catch((err) => {
         return message.channel.send('Database error:\n```js\n' + err + '\n```');
     });
+    }
     
 }
 
