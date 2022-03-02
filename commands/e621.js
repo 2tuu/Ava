59109af721eb2b65bbef3834cfb9b8dc63c38a78
Kab <39675015+2tuu@users.noreply.github.com>
2022-03-02 10:39:04 -0800

    Re-instating NSFW commands with adjustments

---
+++ commands/e621.js
@@ -0,0 +1,90 @@
+const Discord = require("discord.js");
+var axios = require("axios");
+const cooldown = new Set();
+const config = require("./../config.json");
+
+const booru = require('booru');
+
+const taglimit = 10;
+
+exports.run = (client, message, args) => {
+
+    try {
+
+        if (message.channel.nsfw === false) {
+            if (client.isInteraction) {
+                message.reply('NSFW channels only.')
+            }
+            return;
+        } else {
+
+            if (args.length > taglimit) {
+                const embed = new Discord.MessageEmbed()
+                    .setTitle("This site has a limit of " + taglimit + " tags.")
+                return message.channel.send({ embeds: [embed] });
+            }
+
+            async function grab(tags) {
+
+                var result = await booru.search('e621', tags, { limit: 10, random: true })
+                //found commonly disturbing, morally aprehensible, against discord's content rules, or all three!
+                var blacklist = [
+                    "scat",
+                    "watersports",
+                    "gore",
+                    "flash",
+                    "video",
+                    "loli",
+                    "shota",
+                    "cub"
+                ];
+
+                result.posts = result.posts.filter(r => !r.tags.some(e => blacklist.indexOf(e) >= 0)); //Tag blacklist handler
+
+                if (result.posts[0]) {
+                    var url = result.posts[0].fileUrl;
+                    var source = result.posts[0].source;
+
+                    if (!source) {
+                        source = 'No Source Supplied • ' + ' [Post](' + result.posts[0].postView + ')';
+                    } else {
+                        source = '[Source](' + source + ') • [Post](' + result.posts[0].postView + ')';
+                    }
+
+                    const embed = new Discord.MessageEmbed()
+                        .setImage(url)
+                        .setDescription(`${source}`)
+                    message.channel.send({ embeds: [embed] });
+
+                    //console.log(result.posts[0]);
+                } else {
+                    message.channel.send('No results.');
+                }
+            }
+
+            var tags = [];
+
+            if (args[0]) {
+                tags = args;
+            }
+
+            grab(tags);
+
+        }
+
+    } catch (err) {
+        console.error(err);
+    }
+
+}
+
+exports.conf = {
+    category: "NSFW",
+    name: "e621",
+    help: "Search E621",
+    format: "k?e621 [terms]",
+    DM: false,
+    ownerOnly: false,
+    alias: [],
+    slashCommand: false
+}
