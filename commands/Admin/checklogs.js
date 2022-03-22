a8f7c9e65f0267e0f84d2ea82495c012c54dcfad
Kab <39675015+2tuu@users.noreply.github.com>
2022-03-22 10:06:41 -0700

    Added log checker

---
+++ commands/Admin/checklogs.js
@@ -0,0 +1,45 @@
+const Discord = require("discord.js");
+const fs = require(`fs`);
+
+exports.run = async (client, message) => {
+  fs.readdir("./logs/", function (err, files) {
+    var logs = [];
+    files = files.map(function (fileName) {
+      return {
+        name: fileName,
+        time: fs.statSync('./logs/' + fileName).mtime.getTime()
+      };
+    }).sort(function (a, b) { return a.time - b.time; }).map(function (v) { logs.push(v.name); });
+
+    logs = logs.filter(e => (e.endsWith('.log')));
+    logs = logs.reverse();
+    var log = '';
+
+    fs.readFile(`./logs/${logs[0]}`, (error, data) => {
+      if (error) {
+        return console.error(error);
+      }
+
+      var buf = Buffer.from(('LAST LOG:\n\n' + data.toString()), 'utf8');
+
+      message.channel.send({
+        files: [
+          {
+            attachment: buf,
+            name: 'output.log'
+          }
+        ]
+      });
+    });
+
+  })
+}
+
+exports.conf = {
+  name: "N/A (dev command)",
+  help: "Not for you",
+  format: "k?info",
+  DM: true,
+  ownerOnly: true,
+  alias: ['logs']
+}
