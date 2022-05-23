11ada7e0fa9e55d97cac8cc14260cdc6144bf983
Ave <39675015+2tuu@users.noreply.github.com>
2022-05-23 06:20:04 -0700

    Replaced mock command

---
+++ commands/Fun/mock.js
@@ -0,0 +1,36 @@
+exports.run = (client, message, args, deletedMessage, pool, tossedSet, roles, messageContent) => {
+    function mock(str) {
+        str = str.toLowerCase();
+        var splitString = str.split("");
+
+        var i = 0;
+
+        splitString.forEach(e => {
+            var rand = Math.round(Math.random() * 1);
+
+            if(rand === 1){
+                splitString[i] = splitString[i].toUpperCase();
+            }
+
+            i = i + 1;
+        });
+
+        return splitString.join("");
+    }
+
+
+    if (!args[0]) {
+        message.reply("I need text to translate");
+    } else {
+        client.messageHandler(message, client.isInteraction, mock(messageContent));
+    }
+
+}
+
+exports.conf = {
+    help: "Randomly capitalize letters in a given sentance",
+    format: "k?mock [text]",
+    DM: true,
+    OwnerOnly: false,
+    alias: ['spongebob']
+}
