988ff310047ea675faf675dddcd57787ab6db278
Ave <39675015+2tuu@users.noreply.github.com>
2022-07-30 19:09:11 -0700

    Shortened eval filter, removed old ID check

--- commands/Admin/eval.js
+++ commands/Admin/eval.js
@@ -12,52 +12,43 @@ exports.run = (client, message, args, deletedMessage, sql) => {
         return text;
     }
 
-    if (config.evalAllow.includes(message.author.id)) {
+    try {
+      const code = args.join(" ");
+      let evaled = eval(code);
 
-      try {
-        const code = args.join(" ");
-        let evaled = eval(code);
+      if (typeof evaled !== "string")
+        evaled = require("util").inspect(evaled);
 
-        if (typeof evaled !== "string")
-          evaled = require("util").inspect(evaled);
+      var res = clean(evaled);
+      var filter = [config.token_prod, config.token_beta, config.dbpass, config.dbuser, config.kitk_token, config.youtube];
 
-        var res = clean(evaled);
-        res = res.replace(new RegExp(config.token_prod, 'g'), '[TOKEN]');
-        res = res.replace(new RegExp(config.token_beta, 'g'), '[TOKEN]');
+      filter.forEach(e => {
+        res = res.replace(new RegExp(e, 'g'), '[REMOVED]');
+      })
 
-        res = res.replace(new RegExp(config.dbpass, 'g'), '[REDACTED]');
-        res = res.replace(new RegExp(config.dbuser, 'g'), '[REDACTED]');
-        res = res.replace(new RegExp(config.kitk_token, 'g'), '[REDACTED]');
-        res = res.replace(new RegExp(config.youtube, 'g'), '[REDACTED]');
+      if (`\`\`\`\n${res}\n\`\`\``.length > 2000) {
+        var buf = Buffer.from(res, 'utf8');
 
-        if (`\`\`\`\n${res}\n\`\`\``.length > 2000) {
-          var buf = Buffer.from(res, 'utf8');
-
-          const embed = new Discord.MessageEmbed()
-            .setColor(`0x${client.colors.bad}`)
-            .setDescription("```diff\n-Output too long:\n```")
-          message.channel.send({ embeds: [embed] });
-          message.channel.send({
-            files: [
-              {
-                attachment: buf,
-                name: 'log.txt'
-              }
-            ]
-          });
-        } else {
-          client.messageHandler(message, client.isInteraction, `\`\`\`\n${res}\n\`\`\``);
-        }
-      } catch (err) {
-        client.messageHandler(message, client.isInteraction, `\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+        const embed = new Discord.MessageEmbed()
+          .setColor(`0x${client.colors.bad}`)
+          .setDescription("```diff\n-Output too long:\n```")
+        message.channel.send({ embeds: [embed] });
+        message.channel.send({
+          files: [
+            {
+              attachment: buf,
+              name: 'log.txt'
+            }
+          ]
+        });
+      } else {
+        client.messageHandler(message, client.isInteraction, `\`\`\`\n${res}\n\`\`\``);
       }
-
-    } else {
-      const embed = new Discord.MessageEmbed()
-        .setColor(`0x${client.colors.bad}`)
-        .setTitle("You do not have permission to do this. (Bot Owner required)")
-      client.messageHandler(message, client.isInteraction, { embeds: [embed] });
+    } catch (err) {
+      client.messageHandler(message, client.isInteraction, `\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
     }
+
+
   }
 
   evalCMD();
