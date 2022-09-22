cac5ff5697696ff39fb22a2946e181a1ac73cdb2
Ave <39675015+2tuu@users.noreply.github.com>
2022-09-22 07:41:28 -0700

    Fixed snipe command attachment behavior

--- commands/Moderation/snipe.js
+++ commands/Moderation/snipe.js
@@ -5,24 +5,23 @@ exports.run = (client, message, args, deletedMessage) => {
 	if (message.member.permissions.has('MANAGE_MESSAGES') || message.author.id === "378769654942007299") {
 		var channelIDVar = message.channel.id;
 		try {
-			if (args[0]) {
-				var type = deletedMessage[message.guild.id + "-" + args[0].replace("<#", "").replace(">", "")].type;
-				const embed = new Discord.MessageEmbed()
-					.setDescription(deletedMessage[message.guild.id + "-" + channelIDVar].author + ':\n\n' + deletedMessage[message.guild.id + "-" + args[0].replace("<#", "").replace(">", "")].message)
-				client.messageHandler(message, client.isInteraction, { embeds: [embed] });
-				delete deletedMessage[message.guild.id + "-" + args[0].replace("<#", "").replace(">", "")];
-			} else {
-				var type = deletedMessage[message.guild.id + "-" + channelIDVar].type;
-				const embed = new Discord.MessageEmbed()
-					.setDescription(deletedMessage[message.guild.id + "-" + channelIDVar].author + ':\n\n' + deletedMessage[message.guild.id + "-" + channelIDVar].message)
-				client.messageHandler(message, client.isInteraction, { embeds: [embed] });
-				delete deletedMessage[message.guild.id + "-" + channelIDVar];
+				if(deletedMessage[message.guild.id + "-" + channelIDVar].attachments.length > 0){
+					const embed = new Discord.MessageEmbed()
+						.setDescription(deletedMessage[message.guild.id + "-" + channelIDVar].author + ':\n\n' + deletedMessage[message.guild.id + "-" + channelIDVar].message)
+						.setImage(deletedMessage[message.guild.id + "-" + channelIDVar].attachments[0])
+					client.messageHandler(message, client.isInteraction, { embeds: [embed] });
+					delete deletedMessage[message.guild.id + "-" + channelIDVar];
+				} else {
+					const embed = new Discord.MessageEmbed()
+						.setDescription(deletedMessage[message.guild.id + "-" + channelIDVar].author + ':\n\n' + deletedMessage[message.guild.id + "-" + channelIDVar].message)
+					client.messageHandler(message, client.isInteraction, { embeds: [embed] });
+					delete deletedMessage[message.guild.id + "-" + channelIDVar];
+				}
 			}
-		}
 		catch (err) {
 			const embed = new Discord.MessageEmbed()
 				.setColor(`0x${client.colors.bad}`)
-				.setTitle("No deleted messages in this channel")
+				.setTitle("No deleted messages in this channel: " + err)
 			client.messageHandler(message, client.isInteraction, { embeds: [embed] });
 		}
 	}
@@ -33,6 +32,6 @@ exports.conf = {
 	help: "Show the last message that was deleted or edited",
 	format: "k?snipe",
 	DM: false,
-	ownerOnly: true, //fix attachment behavior
+	ownerOnly: false,
 	alias: []
 }
